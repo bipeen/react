7c972651db1ada796e1e599e26bb5cb648818f4f
bipeen <bipeen@gmail.com>
2022-12-26 21:36:38 +0545

    first commit

---
+++ mern-blog/mern.php
@@ -0,0 +1,8 @@
+mern.php
+
+npm create-react-app client
+https://tailwindcss.com/docs/guides/create-react-app
+https://marketplace.visualstudio.com/items?itemName=dsznajder.es7-react-js-snippets
+rafce
+
+npm install react-router-dom
